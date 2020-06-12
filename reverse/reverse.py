6a8840f429f95a4411fc4b055231a547a84e9fb8
Lihuang Zheng <jerryzheng0131@gmail.com>
2020-06-12 11:54:24 -0500

    completed reverse.

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,9 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is not None:
+            if node.next_node is None:
+                self.head = node
+            else:
+                self.reverse_list(node.next_node, node)
+            node.next_node = prev
